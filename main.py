30416bfb87fd5f324a4579cd49d7c967a8fd73f0
Thales Menezes <thlmenezes.dev@gmail.com>
2023-05-27 15:27:13 -0300

    feat: add test pygame file
    https://github.com/kaleblub/pygame-starter-template

---
+++ main.py
@@ -0,0 +1,80 @@
+import pygame
+# ----------- Game Initialization -------------------
+pygame.init()
+
+displayWidth, displayHeight = 700, 800
+
+gameDisplay = pygame.display.set_mode((displayWidth, displayHeight))
+pygame.display.set_caption('Basic Pygame Template')
+clock = pygame.time.Clock()
+
+# ----------- Constants ---------------
+FPS = 60
+backgroundColor = "black"
+
+
+# ----------- Main Game Function ---------------
+def runGame():
+	# Game Variables
+	gameRunning = True
+	gameOver = False
+
+# ----------- Start Of Game Loop ---------------
+	while gameRunning:
+		gameDisplay.fill(backgroundColor)
+
+# ----------- Game Over Menu -------------------
+#	Use this loop as a template for any other screens you want to add.
+		while gameOver == True:
+			pygame.display.update()
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					gameOver = False
+					gameRunning = False
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_q:
+						gameRunning = False
+						gameOver = False
+					if event.key == pygame.K_c:
+						runGame()
+
+# ----------- Gameplay Handling -------------------
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				gameRunning = False
+			if event.type == pygame.MOUSEBUTTONUP:
+				print(f'Mouse was clicked at {pygame.mouse.get_pos()}')
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_LEFT or event.key == ord('a'):
+					print('left')
+				if event.key == pygame.K_RIGHT or event.key == ord('d'):
+					print('right')
+				if event.key == pygame.K_UP or event.key == ord('w'):
+					print('up')
+				if event.key == pygame.K_DOWN or event.key == ord('s'):
+					print('down')
+
+			if event.type == pygame.KEYUP:
+				if event.key == pygame.K_LEFT or event.key == ord('a'):
+					print('left stop')
+				if event.key == pygame.K_RIGHT or event.key == ord('d'):
+					print('right stop')
+				if event.key == pygame.K_UP or event.key == ord('w'):
+					print('up stop')
+				if event.key == pygame.K_DOWN or event.key == ord('s'):
+					print('down stop')
+				if event.key == ord('q'):
+					pygame.quit()
+					gameRunning = False
+					exit()
+
+#  ----------- Game Code -------------------
+		# Draw
+
+		# Update
+		pygame.display.update()
+		clock.tick(FPS)
+
+		
+if __name__ == "__main__":
+	runGame()
