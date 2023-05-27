6afd4a52d011c87a3a7a91d59be38d49a19c528c
Thales Menezes <thlmenezes.dev@gmail.com>
2023-05-27 18:07:58 -0300

    feat: apply some style fixes to GameMenu

--- GameMenu.py
+++ GameMenu.py
@@ -2,9 +2,11 @@ import os
 import pygame
 import Sprite
 
+
 class GameMenu(object):
     """Menu de escolha de personagens"""
-    def __init__(self,jogadores = None):
+
+    def __init__(self, jogadores=None):
         if jogadores is None:
             jogadores = ["", "",
                 [
@@ -20,51 +22,59 @@ class GameMenu(object):
                     pygame.K_DOWN,
                     pygame.K_LEFT,
                     pygame.K_RIGHT,
-                    # NOTE: KP means keypad, changed for testing in more keyboard layouts
-                    pygame.K_7, # pygame.K_KP7,
-                    pygame.K_8, # pygame.K_KP8,
-                    pygame.K_9 #pygame.K_KP9
+                    # NOTE: KP means keypad,
+                    # changed for testing in more keyboard layouts
+                    pygame.K_7,  # pygame.K_KP7,
+                    pygame.K_8,  # pygame.K_KP8,
+                    pygame.K_9   # pygame.K_KP9
                 ]
             ]
         self.inicializa(jogadores)
-    def inicializa(self,jogadores):
+
+    def inicializa(self, jogadores):
         """Inicializa elementos do menu"""
         self.width = pygame.display.get_surface().get_width()
         self.height = pygame.display.get_surface().get_height()
-        pygame.mixer.Sound(os.path.join('.','Sons',"entradamenu.wav")).play()
-        pygame.mixer.music.load(os.path.join('.','Musicas','menu.mp3'))
+        pygame.mixer.Sound(os.path.join('.', 'Sons', "entradamenu.wav")).play()
+        pygame.mixer.music.load(os.path.join('.', 'Musicas', 'menu.mp3'))
         pygame.mixer.music.set_volume(0.3)
         pygame.mixer.music.play(-1)
-        self.font = pygame.font.Font(os.path.join('.','Imagens',"fonte.ttf"), int(self.height*0.08))
+        self.font = pygame.font.Font(os.path.join('.', 'Imagens', "fonte.ttf"),
+                                     int(self.height*0.08))
         retratos = []
         linha = []
-        pos = [self.width*0.2375,self.height*0.45]
-        self.postitulo = [self.width*0.32,self.height*0.34]
+        pos = [self.width*0.2375, self.height*0.45]
+        self.postitulo = [self.width*0.32, self.height*0.34]
         self.cor = 120
         self.incrcor = 3
         self.configcontrole = 0
-        #define posicoes dos retratos na tela
-        for i in [string for string in os.listdir(os.path.join('.','Personagens')) if not string.startswith(".")]:
+        # define posicoes dos retratos na tela
+        for i in [string for string in
+                  os.listdir(os.path.join('.', 'Personagens'))
+                  if not string.startswith(".")]:
             x = pos[0]
             y = pos[1]
-            linha.append(Retrato(i,(x,y)))
+            linha.append(Retrato(i, (x, y)))
             pos[0] = pos[0] + 120
             if (pos[0]+90) >= self.width:
                 pos[1] = pos[1] + 160
                 pos[0] = self.width*0.2375
                 retratos.append(linha[:])
                 linha = []
-                #se o numero de personagens ultrapassa o limite de tamanho da tela para de ler
+                # se o numero de personagens ultrapassa
+                # o limite de tamanho da tela para de ler
                 if (pos[1] + 120) >= self.height:
                     break
-        if len(linha) > 0: retratos.append(linha[:])
+        if len(linha) > 0:
+            retratos.append(linha[:])
         self.retratos = retratos
         self.retratos[0][0].seleciona(1)
         self.retratos[0][0].seleciona(2)
-        self.selecionadop1 = [0,0]
-        self.selecionadop2 = [0,0]
+        self.selecionadop1 = [0, 0]
+        self.selecionadop2 = [0, 0]
         self.numcolunas = int((self.width-50)/120)
-        self.fundo = Sprite.Sprite(os.path.join('.','Imagens','menu.png'),(0,0),0,(self.width,self.height))
+        self.fundo = Sprite.Sprite(os.path.join('.', 'Imagens', 'menu.png'),
+                                   (0,0), 0, (self.width, self.height))
         self.botoesp1 = jogadores[2]
         self.botoesp2 = jogadores[3]
     #eventos de input
@@ -110,7 +120,7 @@ class GameMenu(object):
                         self.retratos[self.selecionadop1[1]][self.selecionadop1[0]].escolhe(1)
                 elif (event.key == self.botoesp1[5]):
                     self.retratos[self.selecionadop1[1]][self.selecionadop1[0]].desescolhe(1)
-                #Controles do P2                    
+                # Controles do P2
                 elif (event.key == self.botoesp2[3]):
                     if not self.retratos[self.selecionadop2[1]][self.selecionadop2[0]].escolhidop2:
                         self.retratos[self.selecionadop2[1]][self.selecionadop2[0]].deseleciona(2)
@@ -150,25 +160,28 @@ class GameMenu(object):
                     self.botoesp2 = []
 
         return False
-        
-    def update(self,dtempo,eventos):
+
+    def update(self, dtempo, eventos):
         """Atualiza elementos do menu"""
         self.cor = self.cor + self.incrcor
         if self.cor >= 255:
             self.incrcor = -3
             self.cor = 255
-        if self.cor <= 100: self.incrcor = 3
-        #caso um jogador esteja configurando os controles
+        if self.cor <= 100:
+            self.incrcor = 3
+        # caso um jogador esteja configurando os controles
         if self.configcontrole == 1:
             for event in eventos:
-                if (event.type == pygame.KEYDOWN): self.botoesp1.append(event.key)
+                if (event.type == pygame.KEYDOWN):
+                    self.botoesp1.append(event.key)
                 if len(self.botoesp1) >= 7:
                     self.botoesp1 = self.botoesp1[:7]
                     self.configcontrole = 0
             return False
         elif self.configcontrole == 2:
             for event in eventos:
-                if (event.type == pygame.KEYDOWN): self.botoesp2.append(event.key)
+                if (event.type == pygame.KEYDOWN):
+                    self.botoesp2.append(event.key)
                 if len(self.botoesp2) >= 7:
                     self.botoesp2 = self.botoesp2[:7]
                     self.configcontrole = 0
@@ -178,9 +191,21 @@ class GameMenu(object):
             for i in self.retratos:
                 for n in i:
                     n.update(dtempo)
-            #Se os 2 jogadores ja escolheram
-            if self.retratos[self.selecionadop1[1]][self.selecionadop1[0]].escolhidop1 and self.retratos[self.selecionadop2[1]][self.selecionadop2[0]].escolhidop2:
-                return [self.retratos[self.selecionadop1[1]][self.selecionadop1[0]].nome,self.retratos[self.selecionadop2[1]][self.selecionadop2[0]].nome,self.botoesp1,self.botoesp2]
+            # Se os 2 jogadores ja escolheram
+            selectp1Y = self.selecionadop1[1]
+            selectp1X = self.selecionadop1[0]
+            retratop1 = self.retratos[selectp1Y][selectp1X]
+
+            selectp2Y = self.selecionadop2[1]
+            selectp2X = self.selecionadop2[0]
+            retratop2 = self.retratos[selectp2Y][selectp2X]
+            if retratop1.escolhidop1 and retratop2.escolhidop2:
+                return [
+                    retratop1.nome,
+                    retratop2.nome,
+                    self.botoesp1,
+                    self.botoesp2
+                ]
             else:
                 return self.input(eventos)
 
@@ -198,7 +223,7 @@ class GameMenu(object):
             screen.blit(superficieescura,(0,0))
             screen.blit(self.font.render("Configurando Controles:", True, (self.cor,0,0)),((self.width/2)-(self.font.size("Configurando Controles:")[0]/2),self.height*0.1))
             screen.blit(self.font.render("Pressione:", True, (self.cor,0,0)),((self.width/2)-(self.font.size("Pressione:")[0]/2),self.height*0.4))
-            
+            listabotoes = [] 
             if self.configcontrole == 1:
                 screen.blit(self.font.render("Jogador 1", True, (self.cor,0,0)),((self.width/2)-(self.font.size("Jogador 1")[0]/2),self.height*0.2))
                 listabotoes = self.botoesp1
